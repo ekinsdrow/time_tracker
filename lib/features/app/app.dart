ab122ba3a60fa517281c31d80b1c5ac03701dff1
Ivan Kolchev <ekinsdrow@gmail.com>
2022-06-15 22:09:06 +0300

    Add user scope and cubit

--- lib/features/app/app.dart
+++ lib/features/app/app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:time_tracker/common/assets/themes/themes.dart';
 import 'package:time_tracker/features/app/router/router.dart';
+import 'package:time_tracker/features/user/di/user_scope.dart';
 
 class App extends StatelessWidget {
   App({Key? key}) : super(key: key);
@@ -9,12 +10,15 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      debugShowCheckedModeBanner: false,
-      routeInformationParser: _router.defaultRouteParser(),
-      routerDelegate: _router.delegate(),
-      title: 'Music App',
-      theme: Themes.lightTheme,
+    return UserScope(
+      router: _router,
+      child: MaterialApp.router(
+        debugShowCheckedModeBanner: false,
+        routeInformationParser: _router.defaultRouteParser(),
+        routerDelegate: _router.delegate(),
+        title: 'Music App',
+        theme: Themes.lightTheme,
+      ),
     );
   }
 }
