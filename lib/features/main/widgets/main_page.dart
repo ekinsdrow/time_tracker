b9579674906e1077b3489fd4f1d2ae19c7dda377
Ivan Kolchev <ekinsdrow@gmail.com>
2022-06-15 22:26:49 +0300

    Add logout button

--- lib/features/main/widgets/main_page.dart
+++ lib/features/main/widgets/main_page.dart
@@ -1,14 +1,22 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class MainPage extends StatelessWidget {
   const MainPage({Key? key}) : super(key: key);
 
+  void _logOut() {
+    FirebaseAuth.instance.signOut();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SafeArea(
         child: Center(
-          child: Text('Main'),
+          child: ElevatedButton(
+            onPressed: _logOut,
+            child: const Text('Log out'),
+          ),
         ),
       ),
     );
