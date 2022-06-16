705fd73e2de64656dca94890e1c9e41b7b4b15b5
Ivan Kolchev <ekinsdrow@gmail.com>
2022-06-16 12:53:50 +0300

    Add pages

---
+++ lib/features/main/features/statistic/widgets/statistic_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class StatisticPage extends StatelessWidget {
+  const StatisticPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Stat'),
+    );
+  }
+}
---
+++ lib/features/main/features/tracker/widgets/tracker_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class TrackerPage extends StatelessWidget {
+  const TrackerPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Tracker'),
+    );
+  }
+}
