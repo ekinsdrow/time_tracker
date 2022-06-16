fe711ab91e88e1db9efbed090b99468d1d4fb403
Ivan Kolchev <ekinsdrow@gmail.com>
2022-06-16 13:09:46 +0300

    Remove unused

--- lib/features/main/widgets/main_page.dart
+++ lib/features/main/widgets/main_page.dart
@@ -1,14 +1,10 @@
 import 'package:auto_route/auto_route.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:time_tracker/features/app/router/router.dart';
 
 class MainPage extends StatelessWidget {
   const MainPage({Key? key}) : super(key: key);
 
-  void _logOut() {
-    FirebaseAuth.instance.signOut();
-  }
 
   @override
   Widget build(BuildContext context) {
