d24824535b601a17bd322123b81ce1e32c6d9822
Ivan Kolchev <ekinsdrow@gmail.com>
2022-06-16 13:09:06 +0300

    Add bottom bar

--- lib/features/main/widgets/main_page.dart
+++ lib/features/main/widgets/main_page.dart
@@ -1,5 +1,7 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:time_tracker/features/app/router/router.dart';
 
 class MainPage extends StatelessWidget {
   const MainPage({Key? key}) : super(key: key);
@@ -10,14 +12,90 @@ class MainPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Center(
-          child: ElevatedButton(
-            onPressed: _logOut,
-            child: const Text('Log out'),
+    return AutoTabsRouter(
+      routes: const [
+        TrackerRouter(),
+        HistoryRouter(),
+        StatisticRouter(),
+      ],
+      builder: (context, child, _) {
+        final tabsRouter = AutoTabsRouter.of(context);
+
+        return Scaffold(
+          body: SafeArea(
+            child: child,
           ),
-        ),
+          bottomNavigationBar: _BottomBar(
+            activeIndex: tabsRouter.activeIndex,
+            callback: (i) {
+              tabsRouter.setActiveIndex(i);
+            },
+          ),
+        );
+      },
+    );
+  }
+}
+
+class _BottomBar extends StatelessWidget {
+  const _BottomBar({
+    required this.activeIndex,
+    required this.callback,
+    Key? key,
+  }) : super(key: key);
+
+  final int activeIndex;
+  final Function(int index) callback;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 60,
+      child: Row(
+        children: [
+          Expanded(
+            child: IconButton(
+              splashRadius: 20,
+              color: activeIndex == 0
+                  ? Theme.of(context).primaryColor
+                  : Theme.of(context)
+                      .bottomNavigationBarTheme
+                      .unselectedItemColor,
+              onPressed: () {
+                callback(0);
+              },
+              icon: const Icon(Icons.timer),
+            ),
+          ),
+          Expanded(
+            child: IconButton(
+              splashRadius: 20,
+              color: activeIndex == 1
+                  ? Theme.of(context).primaryColor
+                  : Theme.of(context)
+                      .bottomNavigationBarTheme
+                      .unselectedItemColor,
+              onPressed: () {
+                callback(1);
+              },
+              icon: const Icon(Icons.history),
+            ),
+          ),
+          Expanded(
+            child: IconButton(
+              splashRadius: 20,
+              color: activeIndex == 2
+                  ? Theme.of(context).primaryColor
+                  : Theme.of(context)
+                      .bottomNavigationBarTheme
+                      .unselectedItemColor,
+              onPressed: () {
+                callback(2);
+              },
+              icon: const Icon(Icons.auto_graph),
+            ),
+          ),
+        ],
       ),
     );
   }
