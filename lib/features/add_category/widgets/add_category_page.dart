64ca43fc0db609f0d994a66adf08cb4aa3024eea
Ivan Kolchev <ekinsdrow@gmail.com>
2022-06-17 17:46:43 +0300

    Fix add category screen

--- lib/features/add_category/widgets/add_category_page.dart
+++ lib/features/add_category/widgets/add_category_page.dart
@@ -27,55 +27,62 @@ class _AddCategoryPageState extends State<AddCategoryPage> {
       body: Container(
         padding: const EdgeInsets.symmetric(
           horizontal: Constants.mediumPadding,
+          vertical: Constants.mediumPadding,
         ),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Container(
-              decoration: BoxDecoration(
-                border: Border.all(
-                  width: 1,
-                  color: Theme.of(context).hintColor,
+            Column(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      width: 1,
+                      color: Theme.of(context).hintColor,
+                    ),
+                    borderRadius: BorderRadius.circular(
+                      Constants.smallPadding,
+                    ),
+                  ),
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: Constants.smallPadding,
+                  ),
+                  width: double.infinity,
+                  child: DropdownButtonHideUnderline(
+                    child: DropdownButton<int>(
+                      value: _dropDownIndex,
+                      isExpanded: true,
+                      items: [
+                        for (var i = 0; i < 10; i++)
+                          DropdownMenuItem(
+                            child: Text('Music $i'),
+                            value: i,
+                          ),
+                      ],
+                      onChanged: (v) {
+                        setState(() {
+                          if (v != null) {
+                            _dropDownIndex = v;
+                          }
+                        });
+                      },
+                    ),
+                  ),
                 ),
-                borderRadius: BorderRadius.circular(
-                  Constants.smallPadding,
+                const SizedBox(
+                  height: Constants.mediumPadding,
                 ),
-              ),
-              padding: const EdgeInsets.symmetric(
-                horizontal: Constants.smallPadding,
-              ),
-              width: double.infinity,
-              child: DropdownButtonHideUnderline(
-                child: DropdownButton<int>(
-                  value: _dropDownIndex,
-                  isExpanded: true,
-                  items: [
-                    for (var i = 0; i < 10; i++)
-                      DropdownMenuItem(
-                        child: Text('Music $i'),
-                        value: i,
-                      ),
-                  ],
-                  onChanged: (v) {
-                    setState(() {
-                      if (v != null) {
-                        _dropDownIndex = v;
-                      }
-                    });
-                  },
+                TextField(
+                  controller: _controller,
                 ),
-              ),
-            ),
-            const SizedBox(
-              height: Constants.mediumPadding,
-            ),
-            TextField(
-              controller: _controller,
-            ),
-            const SizedBox(
-              height: Constants.mediumPadding,
+                const SizedBox(
+                  height: Constants.mediumPadding,
+                ),
+              ],
             ),
             SizedBox(
               width: double.infinity,
+              height: 50,
               child: ElevatedButton(
                 onPressed: _save,
                 child: const Text('Save'),
