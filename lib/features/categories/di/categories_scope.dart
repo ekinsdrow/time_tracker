13effa663526821025d3aa2260edd23f8c97499c
Ivan Kolchev <ekinsdrow@gmail.com>
2022-06-28 20:53:43 +0300

    Change di

--- lib/features/categories/di/categories_scope.dart
+++ lib/features/categories/di/categories_scope.dart
@@ -3,8 +3,10 @@ import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/provider.dart';
+import 'package:time_tracker/common/assets/constants.dart';
 import 'package:time_tracker/common/dialogs/error_dialog.dart';
 import 'package:time_tracker/features/categories/bloc/categories_bloc.dart';
+import 'package:time_tracker/features/categories/data/models/category.dart';
 import 'package:time_tracker/features/categories/data/repositories/categories_repository.dart';
 
 class CategoriesScope extends StatelessWidget {
@@ -24,14 +26,40 @@ class CategoriesScope extends StatelessWidget {
           categoriesRepository: context.read<ICategoriesRepository>(),
         )..add(const CategoriesEvent.fetch()),
         lazy: false,
-        child: BlocListener<CategoriesBloc, CategoriesState>(
+        child: BlocConsumer<CategoriesBloc, CategoriesState>(
           listener: (context, state) => state.whenOrNull(
             error: (error) => showError(
               context: context,
               error: error,
             ),
           ),
-          child: child,
+          builder: (context, state) => state.when(
+            error: (error) => child,
+            loading: () => Stack(
+              children: [
+                child,
+                Container(
+                  color: Colors.black38,
+                  child: Center(
+                    child: Container(
+                      padding: const EdgeInsets.all(
+                        Constants.smallPadding,
+                      ),
+                      decoration: BoxDecoration(
+                        color: Theme.of(context).scaffoldBackgroundColor,
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: const CircularProgressIndicator(),
+                    ),
+                  ),
+                )
+              ],
+            ),
+            success: (categories) => Provider<List<Category>>(
+              create: (context) => categories,
+              child: child,
+            ),
+          ),
         ),
       ),
     );
