291e2d89404b92e7551accd26113d40d2fb0b917
Ivan Kolchev <ekinsdrow@gmail.com>
2022-07-04 13:46:54 +0300

    Add models

---
+++ lib/features/main/features/history/data/models/activity.dart
@@ -0,0 +1,16 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'activity.freezed.dart';
+part 'activity.g.dart';
+
+@freezed
+class Activity with _$Activity {
+  factory Activity({
+    required String categoryId,
+    required int duration,
+    required DateTime dateTime,
+  }) = _Activity;
+
+  factory Activity.fromJson(Map<String, dynamic> json) =>
+      _$ActivityFromJson(json);
+}
