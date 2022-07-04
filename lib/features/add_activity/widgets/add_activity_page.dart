5a2a2b94e7263c40398adf82e0cb7e700c141ccc
Ivan Kolchev <ekinsdrow@gmail.com>
2022-07-04 11:54:17 +0300

    Add page

---
+++ lib/features/add_activity/widgets/add_activity_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AddActivityPage extends StatelessWidget {
+  const AddActivityPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SafeArea(
+        child: Text('Add'),
+      ),
+    );
+  }
+}
