a493bc87bb5f6ad87a17e7ade891f98e4718188c
Ivan Kolchev <ekinsdrow@gmail.com>
2022-07-04 14:08:04 +0300

    Add error handle

--- lib/features/main/features/history/di/history_scope.dart
+++ lib/features/main/features/history/di/history_scope.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:time_tracker/common/dialogs/error_dialog.dart';
 import 'package:time_tracker/features/main/features/history/bloc/history_bloc.dart';
 import 'package:time_tracker/features/main/features/history/data/repository/history_repository.dart';
 
@@ -19,7 +20,12 @@ class HistoryScope extends StatelessWidget {
         create: (context) => HistoryBloc(
           historyRepository: context.read<IHistoryRepository>(),
         ),
-        child: child,
+        child: BlocListener<HistoryBloc, HistoryState>(
+          listener: (context, state) => state.whenOrNull(
+            error: (error) => showError(context: context, error: error),
+          ),
+          child: child,
+        ),
       ),
     );
   }
