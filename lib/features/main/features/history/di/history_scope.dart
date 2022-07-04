0a150bdf0b8fa739483546b92a6f87ba81a93561
Ivan Kolchev <ekinsdrow@gmail.com>
2022-07-04 14:01:09 +0300

    Add scope

---
+++ lib/features/main/features/history/di/history_scope.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:time_tracker/features/main/features/history/bloc/history_bloc.dart';
+import 'package:time_tracker/features/main/features/history/data/repository/history_repository.dart';
+
+class HistoryScope extends StatelessWidget {
+  const HistoryScope({
+    required this.child,
+    Key? key,
+  }) : super(key: key);
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return RepositoryProvider<IHistoryRepository>(
+      create: (context) => HistoryRepository(),
+      child: BlocProvider(
+        create: (context) => HistoryBloc(
+          historyRepository: context.read<IHistoryRepository>(),
+        ),
+        child: child,
+      ),
+    );
+  }
+}
