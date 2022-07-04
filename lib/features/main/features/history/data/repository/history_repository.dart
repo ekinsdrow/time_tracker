5e1a83cbec19edfe44489d24796af9d46eb7f3f6
Ivan Kolchev <ekinsdrow@gmail.com>
2022-07-04 13:57:33 +0300

    Add repo

---
+++ lib/features/main/features/history/data/repository/history_repository.dart
@@ -0,0 +1,36 @@
+import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:time_tracker/features/main/features/history/data/models/activity.dart';
+
+abstract class IHistoryRepository {
+  Stream<List<Activity>> getActivities({
+    required String userId,
+  });
+}
+
+class HistoryRepository implements IHistoryRepository {
+  @override
+  Stream<List<Activity>> getActivities({required String userId}) {
+    final snapshot =
+        FirebaseFirestore.instance.collection('activities').snapshots();
+
+    return snapshot.transform(
+      StreamTransformer.fromHandlers(
+        handleData: (data, sink) {
+          final activities = <Activity>[];
+          for (final activity in data.docs) {
+            final data = activity.data();
+            data['id'] = activity.id;
+
+            activities.add(
+              Activity.fromJson(data),
+            );
+          }
+
+          sink.add(activities);
+        },
+      ),
+    );
+  }
+}
