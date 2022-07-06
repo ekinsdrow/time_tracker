5cc6e46444520ab61e4a95329521c4c3e7778e90
Ivan Kolchev <ekinsdrow@gmail.com>
2022-07-06 13:54:51 +0300

    Add scope

---
+++ lib/features/main/features/statistic/di/statistic_scope.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:provider/provider.dart';
+import 'package:time_tracker/features/activity/data/models/activity.dart';
+import 'package:time_tracker/features/categories/data/models/categories.dart';
+import 'package:time_tracker/features/main/features/statistic/bloc/statistic_bloc.dart';
+
+class StatisticScope extends StatelessWidget {
+  const StatisticScope({
+    required this.child,
+    Key? key,
+  }) : super(key: key);
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => StatisticBloc(
+        activities: context.read<List<Activity>>(),
+        categories: context.read<Categories>().categories,
+      ),
+      child: BlocBuilder<StatisticBloc, StatisticState>(
+        builder: (context, state) => state.when(
+          statistic: (categories) => Provider.value(
+            value: categories,
+            child: child,
+          ),
+        ),
+      ),
+    );
+  }
+}
