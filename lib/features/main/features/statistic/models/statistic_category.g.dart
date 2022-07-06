b65c38563171eb0be0fa64874782f1c6c03c4741
Ivan Kolchev <ekinsdrow@gmail.com>
2022-07-06 14:25:27 +0300

    Add id

--- lib/features/main/features/statistic/models/statistic_category.g.dart
+++ lib/features/main/features/statistic/models/statistic_category.g.dart
@@ -9,6 +9,7 @@ part of 'statistic_category.dart';
 _$_StatisticCategory _$$_StatisticCategoryFromJson(Map<String, dynamic> json) =>
     _$_StatisticCategory(
       title: json['title'] as String,
+      id: json['id'] as String,
       time: Time.fromJson(json['time'] as Map<String, dynamic>),
       subCategories: (json['subCategories'] as List<dynamic>)
           .map((e) => StatisticCategory.fromJson(e as Map<String, dynamic>))
@@ -19,6 +20,7 @@ Map<String, dynamic> _$$_StatisticCategoryToJson(
         _$_StatisticCategory instance) =>
     <String, dynamic>{
       'title': instance.title,
+      'id': instance.id,
       'time': instance.time,
       'subCategories': instance.subCategories,
     };
