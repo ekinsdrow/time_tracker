80173cdda80636ffbbc26aa14c8316f0af8452ac
Ivan Kolchev <ekinsdrow@gmail.com>
2022-07-11 19:00:12 +0300

    Change bloc

--- lib/features/import/bloc/import_bloc.dart
+++ lib/features/import/bloc/import_bloc.dart
@@ -1,6 +1,7 @@
 import 'package:bloc/bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:time_tracker/features/activity/data/models/activity.dart';
+import 'package:time_tracker/features/categories/data/models/category_leaf.dart';
 import 'package:time_tracker/features/import/data/import_repository.dart';
 
 part 'import_event.dart';
@@ -24,6 +25,8 @@ class ImportBloc extends Bloc<ImportEvent, ImportState> {
     try {
       await importRepository.save(
         activities: event.activities,
+        categories: event.categories,
+        userId: event.userId,
       );
 
       emit(const _Sucess());
