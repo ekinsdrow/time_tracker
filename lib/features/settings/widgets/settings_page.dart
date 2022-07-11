3bf18e2e2980c4e07e08885da0d432ad5a10c866
Ivan Kolchev <ekinsdrow@gmail.com>
2022-07-11 18:14:29 +0300

    Add activity item

--- lib/features/settings/widgets/settings_page.dart
+++ lib/features/settings/widgets/settings_page.dart
@@ -1,6 +1,9 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:time_tracker/common/assets/constants.dart';
+import 'package:time_tracker/features/app/router/router.dart';
 import 'package:time_tracker/features/settings/bloc/settings_bloc.dart';
 import 'package:time_tracker/features/settings/di/settings_scope.dart';
 
@@ -17,27 +20,55 @@ class SettingsPage extends StatelessWidget {
             body: SafeArea(
               child: Column(
                 children: [
-                  InkWell(
-                    onTap: () {
+                  _Item(
+                    callback: () {
                       context.read<SettingsBloc>().add(
                             const SettingsEvent.logout(),
                           );
                     },
-                    child: Container(
-                      padding: const EdgeInsets.all(
-                        Constants.mediumPadding,
-                      ),
-                      width: double.infinity,
-                      child: const Text(
-                        'Log out',
-                      ),
-                    ),
+                    text: 'Log out',
                   ),
+                  if (!kReleaseMode)
+                    _Item(
+                      callback: () {
+                        context.router.push(
+                          const ImportRoute(),
+                        );
+                      },
+                      text: 'Open import screen',
+                    ),
                 ],
               ),
             ),
           );
         },
+      ),
+    );
+  }
+}
+
+class _Item extends StatelessWidget {
+  const _Item({
+    required this.callback,
+    required this.text,
+    Key? key,
+  }) : super(key: key);
+
+  final String text;
+  final VoidCallback callback;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: callback,
+      child: Container(
+        padding: const EdgeInsets.all(
+          Constants.mediumPadding,
+        ),
+        width: double.infinity,
+        child: Text(
+          text,
+        ),
       ),
     );
   }
