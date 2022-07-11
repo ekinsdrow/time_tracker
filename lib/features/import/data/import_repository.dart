b2cf73abbfe8fc48e90cd35123cfcaf8f41eca03
Ivan Kolchev <ekinsdrow@gmail.com>
2022-07-11 18:59:53 +0300

    Change repo

--- lib/features/import/data/import_repository.dart
+++ lib/features/import/data/import_repository.dart
@@ -6,7 +6,9 @@ import 'package:time_tracker/features/categories/data/models/category_leaf.dart'
 
 abstract class IImportRepository {
   Future<void> save({
+    required List<CategoryLeaf> categories,
     required List<Activity> activities,
+    required String userId,
   });
 }
 
@@ -14,23 +16,61 @@ class ImportRepository implements IImportRepository {
   @override
   Future<void> save({
     required List<Activity> activities,
+    required List<CategoryLeaf> categories,
+    required String userId,
   }) async {
     final db = FirebaseFirestore.instance;
-    final batch = db.batch();
 
-    for (final act in activities) {
+    final catMap = <String, int>{};
+
+    //Update activities
+    var batch = db.batch();
+
+    for (int j = 0; j < activities.length; j++) {
+      final act = activities[j];
       final activity = db.collection('activities').doc();
+
+      final map = <String, dynamic>{
+        'userId': userId,
+      }..addAll(act.toJson());
 
       batch.set(
         activity,
-        act.toJson(),
+        map,
       );
 
-      final category = db.collection('categories').doc(act.categoryId);
+      catMap[act.categoryId] = (catMap[act.categoryId] ?? 0) + act.duration;
 
-      log(category.toString());
+      if (0 == j % 500) {
+        batch.commit();
+        batch = db.batch();
+      }
     }
 
-    // return await batch.commit();
+    await batch.commit();
+
+    //Update categories
+
+    final batchCat = db.batch();
+    for (final category in categories) {
+      for (final sub in category.subCategories) {
+        if (catMap.keys.contains(sub.id)) {
+          catMap[category.id] = (catMap[category.id] ?? 0) + catMap[sub.id]!;
+        }
+      }
+    }
+
+    for (final key in catMap.keys) {
+      final document = db.collection('categories').doc(key);
+
+      batchCat.update(
+        document,
+        {
+          'allDuration': catMap[key]!,
+        },
+      );
+    }
+
+    await batchCat.commit();
   }
 }
