3af6b6657c1036c848368c48076a23f866bb7c2e
Ivan Kolchev <ekinsdrow@gmail.com>
2022-07-12 22:21:46 +0300

    Slice big text

--- lib/common/extensions/string.dart
+++ lib/common/extensions/string.dart
@@ -2,4 +2,12 @@ extension StringExt on String {
   String get capitalize {
     return '${this[0].toUpperCase()}${substring(1)}';
   }
+
+  String get slice {
+    if (length < 10) {
+      return this;
+    }
+
+    return substring(0, 9) + ' ...';
+  }
 }
