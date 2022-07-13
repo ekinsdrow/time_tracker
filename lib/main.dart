86e2474c9c2518f9a6063fac5962a87800b8ec34
Ivan Kolchev <ekinsdrow@gmail.com>
2022-07-13 15:33:08 +0300

    Remove todos and fix problem

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:time_tracker/features/app/app.dart';
 import 'package:time_tracker/firebase_options.dart';
 
-//TODO: change name and icon
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
