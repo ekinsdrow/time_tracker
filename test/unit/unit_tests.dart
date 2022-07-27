ed7a5759e47352d7fa7271fc8956fb961c3eeb72
Ivan Kolchev <ekinsdrow@gmail.com>
2022-07-27 13:47:28 +0300

    Remove unused file

--- test/unit/unit_tests.dart
+++ test/unit/unit_tests.dart
@@ -1,9 +1,23 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'extensions/extension_tests.dart';
+import 'extensions/date_time_ext_test.dart';
+import 'extensions/duration_ext_test.dart';
+import 'extensions/int_ext_test.dart';
+import 'extensions/string_ext_test.dart';
 
 void unitTests() {
   group('Unit', () {
-    extensionsTests();
+    group(
+      'Extensions',
+      () {
+        dateTimeTest();
+
+        durationExtTest();
+
+        intExtTest();
+
+        staringExtTest();
+      },
+    );
   });
 }
